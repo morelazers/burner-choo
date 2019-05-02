80803c4bafd85d44e487e2c3584dc82266870ee7
Tom Nash <contacttomnash@gmail.com>
2019-05-02 13:57:43 +1000

    BACK BUTTERNS EVERYWHERE

--- views/dapps/poop.js
+++ views/dapps/poop.js
@@ -16,10 +16,11 @@ module.exports = (state, emit) => {
       <div class="w-100 tc">
         <div class="f1">F A R T   N O I S E</div>
         <img class="" src="/assets/dapps/poop/icon.png" />
-        <div class="f3">Pay ${state.CURRENCY_SYMBOL}${poop.price} to make everyone else's phone fart</div>
+        <div class="f3">Pay ${state.CURRENCY_SYMBOL}${poop.price} to make everyone else's phone fart (might only work if they have the wallet open don't @me).</div>
       </div>
-      <div class="actions">
+      <div class="actions flex flex-column items-center">
         <a class="post-file" onclick=${fart}>${poop.farting ? 'NICE' : 'DO IT'}</a>
+        <a onclick=${() => emit('replaceState', '/dapps')}>BACK</a>
       </div>
     </section>
   `
